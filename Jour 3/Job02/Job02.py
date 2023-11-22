41130be943e8008b1af6ceb0623a87890ffebaf7
clement-morier <clement.morier@gmail.com>
2023-11-22 10:20:30 +0100

    fini

---
+++ Jour 3/Job02/Job02.py
@@ -0,0 +1,7 @@
+def My_print_name (name):
+    print(name)
+
+
+My_print_name('Clément')
+My_print_name('11')
+My_print_name('Salut je suis')
---
+++ Jour 3/Job03/Job03.py
@@ -0,0 +1,6 @@
+def Add(a,b):
+    print(a+b)
+
+Add(1,2)
+Add(50,85)
+Add(1821522,1522186)
